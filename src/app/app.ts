5c1407b7291bf095fdd721a132df21b6e31a5363
tsy <syck.teoh@gmail.com>
2026-09-05 13:10:55 +0800

    fix dih problems

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Homepage } from './pages/homepage/homepage';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Homepage],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
